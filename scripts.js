60ce12eca360aeab41a6cd98fdf532f6520adf47
Steven <steventat@csus.edu>
2020-04-25 12:43:04 -0700

    added questions page and additional pages.

--- scripts.js
+++ scripts.js
@@ -3,11 +3,17 @@ function onSignIn(googleUser) {
   console.log('Name: ' + profile.getName());
   console.log('Image URL: ' + profile.getImageUrl());
   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  window.location.href = "./questions.html";
 }
 
 function signOut() {
   var auth2 = gapi.auth2.getAuthInstance();
   auth2.signOut().then(function () {
     console.log('User signed out.');
+    window.location.href = "./index.html";
   });
 }
+
+//Jquery
+$(document).ready(function(){
+});
